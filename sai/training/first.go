6dd0a52892d0472c31fbaea1d442aa5b562c6af4
Sai Kumar Basaveswara <saikumar@Sais-MBP.lan>
2022-02-23 21:32:53 +0530

    added initial files in my folder(sai)

---
+++ sai/training/first.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Sai"
+	homeTown := "Hyderabad"
+	num := 8
+	isWeatherAmazing := true
+	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.\n", name, homeTown, num, isWeatherAmazing)
+}
