9b4671b928635c22304a3c348f7648b965cfc3e4
ishan chaudhary <ishan4220@gmail.com>
2022-02-23 21:38:14 +0530

    Training Files

---
+++ ishan/training/Fundamentals/main.go
@@ -0,0 +1,31 @@
+package main
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	// var ages [3]int = [3]int{20, 25, 23}
+	// fmt.Println(ages, len(ages))
+
+	// names := [4]string{"ishan", "mini", "kirti", "juhi"}
+	// fmt.Println(names)
+
+	// scores := []int{100, 50, 60}
+	// fmt.Println(scores)
+
+	// for _, value := range names {
+	// 	fmt.Printf("the value is %v\n", value)
+	// }
+
+	// for _, value := range points {
+	// 	fmt.Println(value)
+	// }
+
+	// mybill := createNewUser("Ishan", "ishan@123", "123", "India")
+
+	// fmt.Println(mybill.getUserInfo())
+	// fmt.Println(logInUser("ishan@123", "123"))
+	// fmt.Println(logInUser("ishan@123", "1234"))
+	createBill()
+}
