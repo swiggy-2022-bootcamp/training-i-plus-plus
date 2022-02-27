8e2c8560e60983a4c9a62b8a2ad88c466c1db756
Rishabh Mishra <rishabhmishra3321@gmail.com>
2022-02-28 03:41:10 +0530

    Go Essentials

---
+++ rishabh/training/5_if_else/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var isLoggedIn bool = true
+	if isLoggedIn{
+		fmt.Println("User is logged in");
+	}else{
+		fmt.Println("User is logged in");	
+	}
+}
---
+++ rishabh/training/9_map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	library := make(map[string]string)
+
+	library["Book-1"] = "Author-1"
+	library["Book-2"] = "Author-2"
+	library["Book-3"] = "Author-3"
+
+	fmt.Println("Library :",library)
+	book4,isPresent := library["Book-4"]
+	fmt.Println(isPresent,book4)
+}
