94f63a69849a1af6f4e3a4c6b3852172aa18967c
Aaditya <aadityakhetan123@gmail.com>
2022-02-27 20:29:58 +0530

    File handling, defer keyword, packages.

---
+++ aaditya/mini-project/healthcareApp/service/fileOps.go
@@ -0,0 +1,94 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"healthcareApp/model"
+	"log"
+	"os"
+)
+
+func init() {
+    file, err := os.Create("data/patient.json")
+     
+    if err != nil {
+        log.Fatalf("failed creating file: %s", err)
+    }
+
+	file.Close()
+
+	file, err = os.Create("data/generalUser.json")
+     
+    if err != nil {
+        log.Fatalf("failed creating file: %s", err)
+    }
+
+	file.Close()
+
+	file, err = os.Create("data/doctor.json")
+     
+    if err != nil {
+        log.Fatalf("failed creating file: %s", err)
+    }
+
+	file.Close()
+}
+
+type GeneralUser model.GeneralUser
+type Doctor model.Doctor
+type Patient model.Patient
+
+type FileWriter interface {
+	WriteDataToFile()
+}
+
+type FileReader interface {
+	ReadDataFromFile()
+}
+
+
+func (generalUser GeneralUser) WriteDataToFile(){
+	file,err := os.OpenFile("data/generalUser.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	data, _ := json.Marshal(generalUser)
+	_,err = file.WriteString(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Println("Data written to file successfully.")
+	}
+}
+
+
+func (doctor Doctor) WriteDataToFile(){
+	file,err := os.OpenFile("data/doctor.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	data, _ := json.Marshal(doctor)
+	_,err = file.WriteString(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Println("Data written to file successfully.")
+	}
+}
+
+func (patient Patient) WriteDataToFile(){
+	file,err := os.OpenFile("data/patient.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	data, _ := json.Marshal(patient)
+	_,err = file.WriteString(string(data))
+	if err != nil {
+		log.Fatal(err)
+	}else{
+		fmt.Println("Data written to file successfully.")
+	}
+}
