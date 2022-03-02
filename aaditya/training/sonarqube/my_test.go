5bf40c8109593bb2f33a88d10a872d9f7724be44
Aaditya <aadityakhetan123@gmail.com>
2022-03-02 20:37:07 +0530

    setup sonarqube, sonarqube-scanner and ran go test

---
+++ aaditya/training/sonarqube/my_test.go
@@ -0,0 +1,23 @@
+package Test
+
+
+import (
+
+	"testing"
+
+
+	"github.com/stretchr/testify/assert"
+
+)
+
+
+func TestDemo(t *testing.T) {
+
+	one := "one"
+
+	two := "one"
+
+
+	assert.Equal(t, one, two, "the two variables should be the same value")
+
+}
