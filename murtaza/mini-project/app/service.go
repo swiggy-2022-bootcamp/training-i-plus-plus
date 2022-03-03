2fee6230f0bc01ae5f4c20d6b228c9d93855fbb1
Murtaza Personal <murtaza896@MBP-M1-8.local>
2022-03-03 23:40:34 +0530

    added test cases | <Murtaza>

--- murtaza/mini-project/app/service.go
+++ murtaza/mini-project/app/service.go
@@ -10,23 +10,112 @@ var secret string = "Alohomora"
 
 type Service interface {
 	AuthenticateUser(string, string) (string, error)
-	VerifyToken(string) (int, error)
+	ParseAuthToken(string) (int, role, error)
 
 	GetAllUsers() []User
 	GetUserByUsername(string) User
-	AddUser() (bool, error)
-	DeleteUserByUsername(string)
+	GetUserByEmail(string) User
+	AddUser(User) (bool, error)
+	DeleteUserByUsername(string) (bool, error)
+
+	SetEmail(string)
+	SetUsername(string)
+	SetPassword(string)
+	SetPhone(string)
+	SetFirstName(string)
+	SetLastName(string)
+}
+
+type role int
+
+const (
+	Admin    role = iota + 1 // EnumIndex = 1
+	Seller                   // EnumIndex = 2
+	Customer                 // EnumIndex = 3
+)
+
+func (r role) String() string {
+	return [...]string{"admin", "seller", "customer"}[r-1]
+}
+
+func (r role) EnumIndex() int {
+	return int(r)
+}
+
+func GetEnumByIndex(idx int) role {
+	switch idx {
+	case 1:
+		return Admin
+	case 2:
+		return Seller
+	case 3:
+		return Customer
+	default:
+		panic("invalid enum index")
+	}
 }
 
 type User struct {
-	Id        int
-	FirstName string
-	LastName  string
-	Username  string
-	Password  string
-	Phone     string
-	Email     string
+	id        int
+	firstName string
+	lastName  string
+	username  string
+	password  string
+	phone     string
+	email     string
+	role      role
 }
+
+//---- getter and setter --------
+func (u *User) GetEmail() string {
+	return u.email
+}
+
+func (u *User) SetEmail(email string) {
+	u.email = email
+}
+
+func (u *User) GetFirstName() string {
+	return u.firstName
+}
+
+func (u *User) SetFirstName(firstName string) {
+	u.firstName = firstName
+}
+
+func (u *User) GetLastName() string {
+	return u.lastName
+}
+
+func (u *User) SetLastName(lastName string) {
+	u.lastName = lastName
+}
+
+func (u User) GetUsername() string {
+	return u.username
+}
+
+func (u *User) SetUsername(username string) {
+	u.username = username
+}
+
+func (u User) GetPassword() string {
+	return u.password
+}
+
+func (u *User) SetPassword(password string) {
+	u.password = password
+}
+
+func (u User) GetPhone() string {
+	return u.phone
+}
+
+func (u *User) SetPhone(phone string) {
+	u.phone = phone
+}
+
+//-------------------------------------------
 
 type service struct {
 	db []User
@@ -38,7 +127,16 @@ func (s service) GetAllUsers() []User {
 
 func (s service) GetUserByUsername(username string) (*User, error) {
 	for _, val := range s.GetAllUsers() {
-		if username == val.Username {
+		if username == val.username {
+			return &val, nil
+		}
+	}
+	return nil, fmt.Errorf("no users found")
+}
+
+func (s service) GetUserByEmail(email string) (*User, error) {
+	for _, val := range s.GetAllUsers() {
+		if email == val.email {
 			return &val, nil
 		}
 	}
@@ -51,13 +149,13 @@ func (s service) AuthenticateUser(username string, password string) (string, err
 		panic("something went wrong")
 	}
 
-	if user.Password == password {
-		return generateJWT(user.Id)
+	if user.password == password {
+		return generateJWT(user.id, user.role)
 	}
 	return "", fmt.Errorf("invalid credentials")
 }
 
-func (s service) ParseAuthToken(tokenString string) (float64, error) {
+func (s service) ParseAuthToken(tokenString string) (int, role, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -68,28 +166,48 @@ func (s service) ParseAuthToken(tokenString string) (float64, error) {
 		return []byte(secret), nil
 	})
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims["id"].(float64), nil
+		return int(claims["id"].(float64)), GetEnumByIndex(int(claims["role"].(float64))), nil
 	}
-	return -1, err
+	return -1, -1, err
+}
+
+func (s *service) AddUser(u User) (bool, error) {
+	s.db = append(s.db, u)
+	return true, nil
+}
+
+func (s *service) DeleteUserByUsername(username string) (bool, error) {
+	var pos int = -1
+	users := s.GetAllUsers()
+	for i, v := range users {
+		if v.username == username {
+			pos = i
+			break
+		}
+	}
+	s.db = append(users[0:pos], users[pos+1:]...)
+	return true, nil
 }
 
 func NewService() *service {
 	return &service{
 		db: []User{
 			{
-				FirstName: "Murtaza",
-				LastName:  "Sadriwala",
-				Phone:     "9090887112",
-				Email:     "murtaza896@gmail.com",
-				Password:  "Pass!23",
-				Username:  "murtaza896",
+				firstName: "Murtaza",
+				lastName:  "Sadriwala",
+				phone:     "9090887112",
+				email:     "murtaza896@gmail.com",
+				password:  "Pass!23",
+				username:  "murtaza896",
+				role:      Admin,
 			},
 		},
 	}
 }
 
-func generateJWT(id int) (string, error) {
+func generateJWT(id int, role role) (string, error) {
 	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id": id,
-	}).SignedString([]byte(secret + "sadaldjakl"))
+		"id":   id,
+		"role": role.EnumIndex(),
+	}).SignedString([]byte(secret))
 }
--- murtaza/mini-project/app/service_test.go
+++ murtaza/mini-project/app/service_test.go
@@ -6,9 +6,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var userService = NewService()
+var mockUser = User{
+	firstName: "aryan",
+	lastName:  "singh",
+	username:  "aryan123",
+	password:  "passw0rd",
+	email:     "aryan.singh@gmail.com",
+	phone:     "9900991234",
+	role:      Seller,
+}
 
 func TestAuthenticateUserShouldReturnTokenOnValidCredentials(t *testing.T) {
+	var userService = NewService()
 	actualResponse, err := userService.AuthenticateUser("murtaza896", "Pass!23")
 	assert.Nil(t, err)
 	assert.NotNil(t, actualResponse)
@@ -16,17 +25,130 @@ func TestAuthenticateUserShouldReturnTokenOnValidCredentials(t *testing.T) {
 }
 
 func TestShouldParseValidAuthToken(t *testing.T) {
-	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MH0.hRNAVCB1LFZSCYGW-S8GMZXl1AVAmN2nFZoysNJUBrY"
+	var userService = NewService()
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwicm9sZSI6MX0.EbRVzFSPslw_bhnchRWTGPRNlJnsqWXsbXrirjvA9ss"
 
-	actualResponse, err := userService.ParseAuthToken(tokenString)
-	expectedResponse := float64(0)
+	actualResponse, actualRole, err := userService.ParseAuthToken(tokenString)
+	expectedResponse := int(0)
+	expectedRole := Admin
 	assert.Nil(t, err)
 	assert.Equal(t, expectedResponse, actualResponse)
+	assert.Equal(t, expectedRole, actualRole)
 }
 
 func TestShouldNotParseAuthTokenWithInvalidSecret(t *testing.T) {
+	var userService = NewService()
 	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MH0.WOK8U6-k2NvDSyHMLQM2YJN3whV4LU-OtL2FiRDpZWM"
 
-	_, err := userService.ParseAuthToken(tokenString)
+	_, _, err := userService.ParseAuthToken(tokenString)
 	assert.NotNil(t, err)
+}
+
+func TestShouldAddUser(t *testing.T) {
+	var userService = NewService()
+	newUser := User{
+		firstName: "aryan",
+		lastName:  "singh",
+		username:  "aryan123",
+		password:  "passw0rd",
+		email:     "aryan.singh@gmail.com",
+		phone:     "9900991234",
+		role:      Seller,
+	}
+
+	expectedLength := len(userService.GetAllUsers()) + 1
+	res, _ := userService.AddUser(newUser)
+	actualLength := len(userService.GetAllUsers())
+
+	assert.Equal(t, true, res)
+	assert.Equal(t, expectedLength, actualLength)
+}
+
+func TestShouldDeleteUserByUsername(t *testing.T) {
+	var userService = NewService()
+	var username string = "murtaza896"
+
+	expectedLength := len(userService.GetAllUsers()) - 1
+	res, _ := userService.DeleteUserByUsername(username)
+	actualLength := len(userService.GetAllUsers())
+
+	assert.Equal(t, true, res)
+	assert.Equal(t, expectedLength, actualLength)
+}
+
+func TestShouldReturnUserByUsername(t *testing.T) {
+	var userService = NewService()
+	username := "murtaza896"
+	user, err := userService.GetUserByUsername(username)
+	assert.Equal(t, username, user.GetUsername())
+	assert.NotNil(t, user)
+	assert.Nil(t, err)
+}
+
+func TestShouldReturnErrOnInvalidUsername(t *testing.T) {
+	var userService = NewService()
+	username := "murtaza89612"
+	user, err := userService.GetUserByUsername(username)
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+}
+
+func TestShouldReturnUserByEmail(t *testing.T) {
+	var userService = NewService()
+	email := "murtaza896@gmail.com"
+	user, err := userService.GetUserByEmail(email)
+	assert.Equal(t, email, user.email)
+	assert.NotNil(t, user)
+	assert.Nil(t, err)
+}
+
+func TestShouldReturnErrOnInvalidEmail(t *testing.T) {
+	var userService = NewService()
+	email := "fake.email"
+	user, err := userService.GetUserByEmail(email)
+	assert.Nil(t, user)
+	assert.NotNil(t, err)
+}
+
+func TestShouldUpdateEmail(t *testing.T) {
+	var userService = NewService()
+	newEmail := "abc@abc.com"
+	user := userService.GetAllUsers()[0]
+	user.SetEmail(newEmail)
+	assert.Equal(t, newEmail, user.email)
+}
+
+func TestShouldReturnFirstname(t *testing.T) {
+	expectedFirstName := mockUser.firstName
+	actualFirstName := mockUser.GetFirstName()
+
+	assert.Equal(t, expectedFirstName, actualFirstName)
+}
+
+func TestShouldReturnLastname(t *testing.T) {
+	expectedLastName := mockUser.lastName
+	actualLastName := mockUser.GetLastName()
+
+	assert.Equal(t, expectedLastName, actualLastName)
+}
+
+func TestShouldReturnEmail(t *testing.T) {
+	expectedEmail := mockUser.email
+	actualEmail := mockUser.GetEmail()
+
+	assert.Equal(t, expectedEmail, actualEmail)
+}
+
+func TestShouldReturnPhone(t *testing.T) {
+	expectedPhone := mockUser.phone
+	actualPhone := mockUser.GetPhone()
+
+	assert.Equal(t, expectedPhone, actualPhone)
+}
+
+func TestShouldReturnUsername(t *testing.T) {
+	expectedUsername := mockUser.username
+	actualUsername := mockUser.GetUsername()
+
+	assert.Equal(t, expectedUsername, actualUsername)
 }
