36fa19771a6097f5dc0ed09f814a9def9008a318
Murtaza Personal <murtaza896@MBP-M1-8.local>
2022-03-05 05:55:26 +0530

    cleaned up projrct structure | <Murtaza>

--- murtaza/mini-project/domain/auth_service_test.go
+++ murtaza/mini-project/domain/auth_service_test.go
@@ -42,3 +42,31 @@ func TestShouldParseAuthToken(t *testing.T) {
 	assert.Equal(t, 1, actualId)
 	assert.Equal(t, role, actualRole)
 }
+
+func TestAuthenticateUserShouldReturnTokenOnValidCredentials(t *testing.T) {
+	// var userService = NewService()
+	// actualResponse, err := userService.AuthenticateUser("murtaza896", "Pass!23")
+	// assert.Nil(t, err)
+	// assert.NotNil(t, actualResponse)
+	// assert.True(t, len(actualResponse) > 0)
+}
+
+func TestShouldParseValidAuthToken(t *testing.T) {
+	// var userService = NewService()
+	// tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwicm9sZSI6MX0.EbRVzFSPslw_bhnchRWTGPRNlJnsqWXsbXrirjvA9ss"
+
+	// actualResponse, actualRole, err := userService.ParseAuthToken(tokenString)
+	// expectedResponse := int(0)
+	// expectedRole := Admin
+	// assert.Nil(t, err)
+	// assert.Equal(t, expectedResponse, actualResponse)
+	// assert.Equal(t, expectedRole, actualRole)
+}
+
+func TestShouldNotParseAuthTokenWithInvalidSecret(t *testing.T) {
+	// var userService = NewService()
+	// tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MH0.WOK8U6-k2NvDSyHMLQM2YJN3whV4LU-OtL2FiRDpZWM"
+
+	// _, _, err := userService.ParseAuthToken(tokenString)
+	// assert.NotNil(t, err)
+}
