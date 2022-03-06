a7b1e1fa9aef047ae05583a359583c2c5dd778f5
aky91 <aakash28399@gmail.com>
2022-03-07 00:56:54 +0530

    Login

--- akash/mini-project/main.go
+++ akash/mini-project/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	model "sample.akash.com/model"
+	user "sample.akash.com/user"
 )
 
 func main() {
-	fmt.Println("Hello ", &model.User{"Ash", "Lambert", 20})
+	fmt.Println("Hello ", &model.User{"Ash", "Lambert", "ash.lambert@swiggy.com", "12345"})
+	user.Login("abc", "def")
+
+	r := gin.Default()
+	fmt.Println(r)
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello-world",
+		})
+	})
+	r.Run()
 }
