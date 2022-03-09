cd1809523a0c69569a755ff7002bf62ecf59340c
ayan59dutta <ayan59dutta@gmail.com>
2022-03-09 18:08:44 +0530

    Added tests and main.go

--- ayan/mini-project/shopping-app/user/domain/userService_test.go
+++ ayan/mini-project/shopping-app/user/domain/userService_test.go
@@ -1,1 +1,72 @@
 package domain
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUserService(t *testing.T) {
+
+	testDefaultUserSvc := &DefaultUserService{
+		userDB: nil,
+	}
+
+	newDefaultUserSvc := NewUserService(nil)
+
+	assert.EqualValues(t, testDefaultUserSvc, newDefaultUserSvc)
+}
+
+func TestRegisterForNewUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestRegisterForExistingUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestVerifyCredentialsForGoodCredentials(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestVerifyCredentialsForBadCredentials(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestVerifyUserCredentialsForInvalidUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestFindUserByEmailForPresentUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestFindUserByEmailForNonExistentUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestVerifyTokenForValidUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
+
+func TestVerifyTokenForInvalidUser(t *testing.T) {
+
+	// testUserRepositoryDB := NewUserRepositoryDB([]db.User{})
+
+}
