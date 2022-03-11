da76b755d45c6ef061137ee9bdbe5af45765ebe6
sachin <sachinsom507@gmail.com>
2022-03-11 11:56:39 +0530

    implemented controllers for user CRUD.

--- sachin-som/mini-project/shopping-cart/controllers/user.controller.go
+++ sachin-som/mini-project/shopping-cart/controllers/user.controller.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/sachinsom93/shopping-cart/models"
 	"github.com/sachinsom93/shopping-cart/services"
 )
 
@@ -9,24 +12,67 @@ type UserController struct {
 	UserService services.UserServices
 }
 
+func NewUserController(userService services.UserServices) UserController {
+	return UserController{
+		UserService: userService,
+	}
+}
+
 func (uc *UserController) CreateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.CreateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{"message": "success"})
 }
 
 func (uc *UserController) GetUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	email := ctx.Param("email")
+	user, err := uc.UserService.GetUser(&email)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (uc *UserController) GetAllUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	users, err := uc.UserService.GetAllUser()
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, users)
 }
 
 func (uc *UserController) UpdateUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	err := uc.UserService.UpdateUser(&user)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) DeleteUser(ctx *gin.Context) {
-	ctx.JSON(200, "")
+	email := ctx.Param("email")
+	err := uc.UserService.DeleteUser(&email)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"message": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
 func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
@@ -35,5 +81,5 @@ func (uc *UserController) RegisterUserRoutes(rg *gin.RouterGroup) {
 	userRoute.GET("/get/:email", uc.GetUser)
 	userRoute.GET("/getall", uc.GetAllUser)
 	userRoute.PATCH("/update", uc.UpdateUser)
-	userRoute.DELETE("/delete:email", uc.DeleteUser)
+	userRoute.DELETE("/delete/:email", uc.DeleteUser)
 }
