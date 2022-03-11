d00e97b0462702662eb1f6c94c0412fb8e548067
sachin <sachinsom507@gmail.com>
2022-03-11 11:56:02 +0530

    created services for user CRUD.

--- sachin-som/mini-project/shopping-cart/services/user.service.impl.go
+++ sachin-som/mini-project/shopping-cart/services/user.service.impl.go
@@ -2,8 +2,11 @@ package services
 
 import (
 	"context"
+	"errors"
+	"time"
 
 	"github.com/sachinsom93/shopping-cart/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,21 +23,78 @@ func NewUserService(userCollection *mongo.Collection, ctx context.Context) *User
 }
 
 func (u *UserServiceImplementation) CreateUser(user *models.User) error {
-	return nil
+	_, err := u.userCollection.InsertOne(u.ctx, user)
+	return err
 }
 
 func (u *UserServiceImplementation) GetUser(email *string) (*models.User, error) {
-	return nil, nil
+	var user *models.User
+	query := bson.D{bson.E{Key: "email", Value: email}}
+	err := u.userCollection.FindOne(u.ctx, query).Decode(&user)
+	return user, err
 }
 
-func (u *UserServiceImplementation) GetAllUser() []*models.User {
-	return nil
+func (u *UserServiceImplementation) GetAllUser() ([]*models.User, error) {
+	var users []*models.User
+	cursor, err := u.userCollection.Find(u.ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	for cursor.Next(u.ctx) {
+		var user models.User
+		err := cursor.Decode(&user)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+	cursor.Close(u.ctx)
+	if len(users) == 0 {
+		return nil, errors.New("users not fuond.")
+	}
+	return users, nil
 }
 
 func (u *UserServiceImplementation) UpdateUser(user *models.User) error {
+	filterQuery := bson.D{bson.E{Key: "email", Value: user.Email}}
+	updatedTime, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateQuery := bson.D{
+		bson.E{
+			Key:   "first_name",
+			Value: user.FirstName,
+		},
+		bson.E{
+			Key:   "last_name",
+			Value: user.LastName,
+		},
+		bson.E{
+			Key:   "email",
+			Value: user.Email,
+		},
+		bson.E{
+			Key:   "phone",
+			Value: user.Phone,
+		},
+		bson.E{
+			Key:   "updated_at",
+			Value: updatedTime,
+		},
+	}
+	result, _ := u.userCollection.UpdateOne(u.ctx, filterQuery, updateQuery)
+	if result.MatchedCount != 1 {
+		return errors.New("no matched document found for update.")
+	}
 	return nil
 }
 
 func (u *UserServiceImplementation) DeleteUser(email *string) error {
+	filterQuery := bson.D{bson.E{Key: "email", Value: email}}
+	result, _ := u.userCollection.DeleteOne(u.ctx, filterQuery)
+	if result.DeletedCount != 1 {
+		return errors.New("no matched document found for deleting.")
+	}
 	return nil
 }
