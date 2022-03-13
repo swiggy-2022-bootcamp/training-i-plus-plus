e3142fe248b5f3c09e9737fbce5fd3acbee8a9f1
Udaysonu <udaysonubakka123@gmail.com>
2022-03-13 17:28:44 +0530

    [Project]: Add JWT Authentication

--- uday/mini-project/ShoppingCartProject/server.go
+++ uday/mini-project/ShoppingCartProject/server.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	// "io"
-	// "net/http"
-	// "os"
+import ( 
 	"github.com/gin-gonic/gin"
 	"github.com/Udaysonu/SwiggyGoLangProject/controller"
 	"github.com/Udaysonu/SwiggyGoLangProject/config"
 	"github.com/Udaysonu/SwiggyGoLangProject/service"
+	"github.com/Udaysonu/SwiggyGoLangProject/middleware"
 
 )
 
@@ -16,21 +14,25 @@ var (
 	UEC controller.UserExpertController=config.GetUEC()
 	expertService service.ExpertService=config.GetexpertService()
 	expertController controller.ExpertController=config.GetexpertController()
+	userService service.UserService=service.UserNew()
+	userController controller.UserController=controller.UserNewController(userService)
 )
 
-// func setupLogOutput(){
-// 	f,_:=os.Create("gin.log")
-// 	gin.DefaultWriter = io.MultiWriter(f,os.Stdout)
-// }
 
 func main(){
 
 	expertService.InitDB()
-	server:=gin.Default()
-	server.GET("/services", func(ctx *gin.Context){
-		ctx.JSON(200,expertController.GetSkills())
-	})
 
+	server:=gin.Default()
+
+	server.GET("/services", func(ctx *gin.Context){
+		boolean:=middleware.CheckAuth(ctx,userService)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error, Maybe token expired!"})
+		} else{
+		ctx.JSON(200,expertController.GetSkills())
+		}
+	})
 
 	server.GET("/get",func(ctx *gin.Context){
 		result,err:=expertController.BookEmployee(ctx)
@@ -39,7 +41,6 @@ func main(){
 		} else{
 			ctx.JSON(400,gin.H{"message":"All Expers are busy, please try again"})
 		}
-		
 	})
 
 	server.GET("/done",func(ctx *gin.Context){
@@ -47,16 +48,45 @@ func main(){
 		ctx.JSON(200,gin.H{"message":"Work completed"})
 	})
 
+	server.GET("/experts",func(ctx *gin.Context){
+		ctx.JSON(200,expertController.GetExperts(ctx))
+	})
 
-	
+	server.POST("/addrating",func(ctx *gin.Context){
+		expertController.AddRating(ctx)
+		ctx.JSON(200,gin.H{"message":"Rating Added"})
+	})
 
-	// server.POST("/posts",func(ctx *gin.Context){
-	// 	result,err:=videoController.Save(ctx)
-	// 	if err==nil{
-	// 		ctx.JSON(http.StatusBadRequest,result)
-	// 	} else{
-	// 		ctx.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
-	// 	}
-	// })
+	server.GET("/getexpert",func(ctx *gin.Context){
+		ctx.JSON(200,expertController.GetExpertByID(ctx))
+	})
+
+	server.POST("/signexpert",func(ctx *gin.Context){
+		expertController.DirectSignUp(ctx)
+		ctx.JSON(200,gin.H{"message":"Sign In Successful"})
+	})
+
+	server.GET("/filter",func(ctx *gin.Context){
+		ctx.JSON(200,expertController.FilterExpert(ctx))
+	})
+ 
+	server.POST("/signuser",func(ctx *gin.Context){
+		userController.SignUpUser(ctx)
+		ctx.JSON(200,gin.H{"message":"Sign In Successful"})
+	})
+
+	server.POST("/isuserpresent",func(ctx *gin.Context){
+		ctx.JSON(200,userController.IsUserPresent(ctx))
+	})
+
+	server.POST("/getuser",func(ctx *gin.Context){
+		ctx.JSON(200,userController.GetUser(ctx))
+	})
+
+	server.POST("/loginuser",func(ctx *gin.Context){
+		result:=middleware.Login(ctx,userService)
+		ctx.JSON(200,gin.H{"token":result})
+	})
+
 	server.Run(":8080")
 }
