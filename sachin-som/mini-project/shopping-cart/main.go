e2cdfb455e941469d51b0d43e76941399213458d
sachin <sachinsom507@gmail.com>
2022-03-13 11:58:48 +0530

    Updated main.go

--- sachin-som/mini-project/shopping-cart/main.go
+++ sachin-som/mini-project/shopping-cart/main.go
@@ -6,20 +6,27 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	invcontroller "github.com/sachinsom93/shopping-cart/controllers/inventory"
 	controllers "github.com/sachinsom93/shopping-cart/controllers/user"
 	"github.com/sachinsom93/shopping-cart/database"
+	invservices "github.com/sachinsom93/shopping-cart/services/inventory"
 	services "github.com/sachinsom93/shopping-cart/services/user"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var (
-	server         *gin.Engine
+	err         error
+	ctx         context.Context
+	server      *gin.Engine
+	mongoClient *mongo.Client
+
 	userService    services.UserServices
 	userController controllers.UserController
-	ctx            context.Context
 	userCollection *mongo.Collection
-	mongoClient    *mongo.Client
-	err            error
+
+	inventoryService    invservices.InventoryServices
+	inventoryController invcontroller.InventoryController
+	inventoryCollection *mongo.Collection
 )
 
 func init() {
@@ -38,10 +45,13 @@ func init() {
 
 	// Create Collections
 	userCollection = mongoClient.Database("shopping-cart").Collection("users")
+	inventoryCollection = mongoClient.Database("shopping-cart").Collection("inventories")
 
 	// initiaze layers
 	userService = services.NewUserService(userCollection, ctx)
 	userController = controllers.NewUserController(userService)
+	inventoryService = invservices.NewInventoryService(inventoryCollection, ctx)
+	inventoryController = *invcontroller.NewInventoryController(inventoryService)
 	server = gin.Default()
 }
 
@@ -50,6 +60,7 @@ func main() {
 
 	basePath := server.Group("/v1")
 	userController.RegisterUserRoutes(basePath)
+	inventoryController.RegisterInventoryRoutes(basePath)
 
 	log.Fatal(server.Run(":9090"))
 }
