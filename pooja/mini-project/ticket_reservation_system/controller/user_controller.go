0c05be219aafaab5f43e9f62694d2aef56682216
poojasethia <pooja16041@iiitd.ac.in>
2022-03-15 23:15:43 +0530

    crud operations for user with mongodb

--- pooja/mini-project/ticket_reservation_system/controller/user_controller.go
+++ pooja/mini-project/ticket_reservation_system/controller/user_controller.go
@@ -1,13 +1,18 @@
 package controller
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	db "ticket_reservation_system/database"
 	user_model "ticket_reservation_system/model"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var users []user_model.User
@@ -16,54 +21,75 @@ var userCollection *mongo.Collection = db.DatabaseConn().Database("maindb").Coll
 func AddUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user user_model.User
-	json.NewDecoder(r.Body).Decode(&user)
-	users = append(users, user)
-	resp_string := "user created with username: " + user.UserName
-	json.NewEncoder(w).Encode(resp_string)
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		fmt.Println("error in decoding user payload with error ", err)
+	}
+	insertResult, err := userCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		fmt.Println("error in inserting the data to db ", err)
+	}
+	json.NewEncoder(w).Encode(insertResult.InsertedID)
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
+	var results []primitive.M
+	cur, err := userCollection.Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		fmt.Println(err)
+	}
+	for cur.Next(context.TODO()) {
+		var elem primitive.M
+		if err := cur.Decode(&elem); err != nil {
+			fmt.Println(err)
+		}
+		results = append(results, elem)
+	}
+	cur.Close(context.TODO())
+	json.NewEncoder(w).Encode(results)
 }
 
-func GetUserById(w http.ResponseWriter, r *http.Request) {
+func GetUserByUsername(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	inputUserId := params["user_id"]
-	for _, user := range users {
-		if user.UserId == inputUserId {
-			json.NewEncoder(w).Encode(user)
-			return
-		}
+	var user user_model.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		fmt.Print(err)
 	}
+	var result primitive.M //  an unordered representation of a BSON document which is a Map
+	if err := userCollection.FindOne(context.TODO(), bson.D{{"username", user.UserName}}).Decode(&result); err != nil {
+		fmt.Println(err)
+	}
+	json.NewEncoder(w).Encode(result) // returns a Map containing document
 }
 
-func UpdateUser(w http.ResponseWriter, r *http.Request) {
+func UpdateUserPassword(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	inputUserId := params["user_id"]
-	for i, user := range users {
-		if user.UserId == inputUserId {
-			users = append(users[:i], users[i+1:]...)
-			var updateUser user_model.User
-			json.NewDecoder(r.Body).Decode(&updateUser)
-			users = append(users, updateUser)
-			json.NewEncoder(w).Encode(updateUser)
-			return
-		}
+	type updateBody struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
 	}
+	var body updateBody
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		fmt.Print(err)
+	}
+	filter := bson.D{{"username", body.Username}}
+	after := options.After
+	returnOpt := options.FindOneAndUpdateOptions{
+		ReturnDocument: &after,
+	}
+	update := bson.D{{"$set", bson.D{{"password", body.Password}}}}
+	updateResult := userCollection.FindOneAndUpdate(context.TODO(), filter, update, &returnOpt)
+	var result primitive.M
+	_ = updateResult.Decode(&result)
+	json.NewEncoder(w).Encode(result)
 }
 
-func DeleteUserById(w http.ResponseWriter, r *http.Request) {
+func DeleteUserByUsername(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	inputUserId := params["user_id"]
-	for i, user := range users {
-		if user.UserId == inputUserId {
-			users = append(users[:i], users[i+1:]...)
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
+	username := mux.Vars(r)["username"]
+	opts := options.Delete().SetCollation(&options.Collation{})
+	if _, err := userCollection.DeleteOne(context.TODO(), bson.D{{"username", username}}, opts); err != nil {
+		fmt.Print(err)
 	}
+	json.NewEncoder(w).Encode(http.StatusNoContent)
 }
--- pooja/mini-project/ticket_reservation_system/server.go
+++ pooja/mini-project/ticket_reservation_system/server.go
@@ -12,9 +12,9 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/user", user_controller.AddUser).Methods("POST")
 	router.HandleFunc("/user", user_controller.GetUsers).Methods("GET")
-	router.HandleFunc("/user/{user_id}", user_controller.GetUserById).Methods("GET")
-	router.HandleFunc("/user/{user_id}", user_controller.UpdateUser).Methods("PUT")
-	router.HandleFunc("/user/{user_id}", user_controller.DeleteUserById).Methods("DELETE")
+	router.HandleFunc("/user/{username}", user_controller.GetUserByUsername).Methods("GET")
+	router.HandleFunc("/user/{password}", user_controller.UpdateUserPassword).Methods("PUT")
+	router.HandleFunc("/user/{username}", user_controller.DeleteUserByUsername).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":5001", router))
 }
