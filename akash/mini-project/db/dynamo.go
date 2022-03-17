498f174615e7880755a1408b0b3f573d71179c58
aky91 <aakash28399@gmail.com>
2022-03-17 18:55:26 +0530

    Fixed Delete API and added Update API

--- akash/mini-project/db/dynamo.go
+++
@@ -1,8 +0,0 @@
-package db
-
-// Connect connects to mongodb
-func ConnectDynamoDB() {
-
-}
-
-// docker run --rm -d -p 2717:27017 -v /Users/aky/Dev/swiggy/training-i-plus-plus/akash/training/gin-demo/mongodb-docker:/data/db --name mymongo mongo:latest
