a617b80423afe8b3bf5a69c57f46a49fd5ceec55
Udaysonu <udaysonubakka123@gmail.com>
2022-03-20 16:14:28 +0530

    [Project]: Adding MongoDB to controllers

--- uday/mini-project/ShoppingCartProject/controller/user-expert-controller.go
+++ uday/mini-project/ShoppingCartProject/controller/user-expert-controller.go
@@ -1,40 +1,43 @@
-package controller;
+package controller
+
 import (
 	// "github.com/Udaysonu/SwiggyGoLangProject/entity"
 	"github.com/Udaysonu/SwiggyGoLangProject/service"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-type UserExpertController interface{
-	AddWaitingList(userid int,skill string)
-	CreateRelation(userid int, expertid int,skill string)
-	RemoveRelation(userid int,expertid int) bool
-	AddCost(userid int, expertid int, cost int)
-	MakePayment(userid int, expertid int)
+
+// type UserExpertController interface{
+// 	AddWaitingList(userid int,skill string)
+// 	CreateRelation(userid int, expertid int,skill string)
+// 	RemoveRelation(userid int,expertid int) bool
+// 	AddCost(userid int, expertid int, cost int)
+// 	MakePayment(userid int, expertid int)
+// }
+
+// type userexpertController struct{
+// 	service service.UserExpertService
+// }
+
+// func NewUserExpert(service service.UserExpertService) service.UserExpertService{
+// 	return &userexpertController{service:service}
+// }
+
+func  AddWaitingList(userid primitive.ObjectID,skill string){
+	service.AddWaitingList(userid,skill)
 }
 
-type userexpertController struct{
-	service service.UserExpertService
+func  CreateRelation(userid primitive.ObjectID, expertid primitive.ObjectID,skill string){
+	 service.CreateRelation(userid , expertid ,skill )
 }
 
-func NewUserExpert(service service.UserExpertService) service.UserExpertService{
-	return &userexpertController{service:service}
+func  RemoveRelation(userid primitive.ObjectID,expertid primitive.ObjectID) bool{	 
+	return service.RemoveRelation(userid,expertid)
 }
 
-func (c *userexpertController)AddWaitingList(userid int,skill string){
-	c.service.AddWaitingList(userid,skill)
-}
-
-func (c *userexpertController)CreateRelation(userid int, expertid int,skill string){
-	 c.service.CreateRelation(userid , expertid ,skill )
-}
-
-func (c *userexpertController)RemoveRelation(userid int,expertid int) bool{	 
-	return c.service.RemoveRelation(userid,expertid)
-}
-
- func (c *userexpertController)AddCost(userid int, expertid int, cost int){
-	c.service.AddCost(userid,expertid,cost)
+ func  AddCost(userid primitive.ObjectID, expertid primitive.ObjectID, cost int){
+	service.AddCost(userid,expertid,cost)
  }
 
- func (c *userexpertController)MakePayment(userid int, expertid int){
-	 c.service.MakePayment(expertid,userid)
+ func MakePayment(userid primitive.ObjectID, expertid primitive.ObjectID){
+	 service.MakePayment(expertid,userid)
  }
