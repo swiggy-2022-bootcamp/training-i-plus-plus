c0ad0490c7017888ff3d949960c7cdcc57a408c8
Udaysonu <udaysonubakka123@gmail.com>
2022-03-20 16:17:40 +0530

    [Project]: update services

--- uday/mini-project/ShoppingCartProject/route/expert.go
+++ uday/mini-project/ShoppingCartProject/route/expert.go
@@ -3,65 +3,99 @@ import (
 	"github.com/gin-gonic/gin"
 	 
 	"github.com/Udaysonu/SwiggyGoLangProject/middleware"
- 
+	"github.com/Udaysonu/SwiggyGoLangProject/controller"
 )
 func called(ctx *gin.Context)bool{
 	return true
 }
 
- 
-
 func ExpertRouter(g *gin.RouterGroup){
 	
-	
 	g.GET("/services", func(ctx *gin.Context){
-		boolean:=middleware.CheckAuth(ctx,userService)
+		boolean:=middleware.CheckAuth(ctx)
 		if boolean==false{
 			ctx.JSON(404,gin.H{"message":"Token error, Maybe token expired!"})
 		} else{
-		ctx.JSON(200,expertController.GetSkills())
+		ctx.JSON(200,controller.GetSkills())
 		}
 	})
 
+	g.DELETE("/delete",func(ctx *gin.Context){
+		result:=controller.Delete(ctx)
+		if result==200{
+			ctx.JSON(200,gin.H{"Deleted":"Successfully Deleted"})
+		} else{
+			ctx.JSON(500,gin.H{"Deleted":"Error in deleting"})
+		}
+	})
 
 	g.GET("/get",func(ctx *gin.Context){
-		result,err:=expertController.BookEmployee(ctx)
-		if err==200{
-			ctx.JSON(200,result)
-		} else{
-			ctx.JSON(400,gin.H{"message":"All Expers are busy, please try again"})
+		boolean:=middleware.CheckAuth(ctx)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error! Please check token"})
+		} else {
+			result,err:=controller.BookEmployee(ctx)
+			if err==200{
+				ctx.JSON(200,result)
+			} else{
+				ctx.JSON(400,gin.H{"message":"All Expers are busy, please try again"})
+			}
 		}
 	})
 
+	g.GET("/getallexperts",func(ctx *gin.Context){
+		boolean:=middleware.CheckAuth(ctx)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error! Please check token"})
+		} else {
+			result:=controller.GetAllExperts(ctx)
+			ctx.JSON(200,result)	
+		}
+	})
 
 	g.GET("/done",func(ctx *gin.Context){
-		expertController.WorkDone(ctx)
-		ctx.JSON(200,gin.H{"message":"Work completed"})
+		boolean:=middleware.CheckAuth(ctx)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error! Please check token"})
+		} else {
+			controller.WorkDone(ctx)
+			ctx.JSON(200,gin.H{"message":"Work completed"})		
+		}
 	})
 
 
 	g.GET("/experts",func(ctx *gin.Context){
-		ctx.JSON(200,expertController.GetExperts(ctx))
+		boolean:=middleware.CheckAuth(ctx)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error! Please check token"})
+		} else {
+			ctx.JSON(200,gin.H{"data":controller.GetExperts(ctx)})	
+		}
 	})
 
 
 	g.POST("/addrating",func(ctx *gin.Context){
-		expertController.AddRating(ctx)
+		boolean:=middleware.CheckAuth(ctx)
+		if boolean==false{
+			ctx.JSON(404,gin.H{"message":"Token error! Please check token"})
+		} else {
+		controller.AddRating(ctx)
 		ctx.JSON(200,gin.H{"message":"Rating Added"})
+		}
 	})
 
 
 	g.GET("/getexpert",func(ctx *gin.Context){
-		ctx.JSON(200,expertController.GetExpertByID(ctx))
+		ctx.JSON(200,controller.GetExpertByID(ctx))
 	})
 
 
 	g.POST("/signexpert",func(ctx *gin.Context){
-		expertController.DirectSignUp(ctx)
-		ctx.JSON(200,gin.H{"message":"Sign In Successful"})
+		ctx.JSON(200,gin.H{"message":controller.DirectSignUp(ctx)})
 	})
 
 	g.GET("/filter",func(ctx *gin.Context){
-		ctx.JSON(200,expertController.FilterExpert(ctx))
+		ctx.JSON(200,controller.FilterExpert(ctx))
 	})
+
 }
--- uday/mini-project/ShoppingCartProject/service/user-service.go
+++ uday/mini-project/ShoppingCartProject/service/user-service.go
@@ -1,58 +1,108 @@
 package service
+
 import (
+	"context"
+	"fmt"
+	"log"
+
 	"github.com/Udaysonu/SwiggyGoLangProject/entity"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	// "go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
+	// "github.com/Udaysonu/SwiggyGoLangProject/config"
+	"time"
+
+	"github.com/Udaysonu/SwiggyGoLangProject/database"
 )
 
-type UserService interface{
-	SignUpUser(username string, password string, email string, phone int)
-	IsUserPresent(username string, password string)bool
-	GetUser(username string, password string)entity.User
-	SignIn(username string, password string) entity.User
-}
+ 
+var userCollection *mongo.Collection = database.GetCollection(database.DB, "users")
 
-type userService struct{
-	UserList []entity.User
-}
-
-func UserNew() UserService{
-	return	&userService{[]entity.User{}}
-}
-
-func (s *userService)	SignIn(username string, password string) entity.User{
-	var user entity.User
-	for _,value:=range s.UserList{
-		if value.Username==username && value.Password==password{
-			user=value;
-			break;
-		}
+func GetAllUsers()[]entity.User{
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	var users []entity.User
+	defer cancel()
+	results, _ := userCollection.Find(ctx, bson.M{})
+	for results.Next(ctx) {
+		var singleUser entity.User
+		results.Decode(&singleUser) 
+		users = append(users, singleUser)
 	}
-	return user
-}
-var userid=0
-func (s *userService)SignUpUser(username string, password string, email string, phone int){
-	NewUser:=entity.User{Id:userid,Username:username,Password:password,Email:email,Phone:phone}
- 	s.UserList=append(s.UserList,NewUser)
- 	expertid=expertid+1
+	return users
 }
 
-func (s *userService)IsUserPresent(username string, password string)bool{
+func SignIn(username string, password string) entity.User{
+	var result entity.User
+	 
+	filter := bson.M{"username": username,"password":password}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+ 
+	err := userCollection.FindOne(ctx, filter).Decode(&result)
+	
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	} 
+	return result 
+}
+
+func  SignUpUser(username string, password string, email string, phone int,location int) entity.User{
+	newUser:=entity.User{Id:primitive.NewObjectID(),Username:username,Password:password,Email:email,Phone:phone,Location:location}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	fmt.Println(userCollection)
+	_,err:= userCollection.InsertOne(ctx, newUser)
+	 if err!=nil{
+		 fmt.Println(err)
+	 }
+	return newUser 
+}
+
+func IsUserPresent(username string, password string)bool{
 	var isPresent bool=false;
-	for _,value:=range s.UserList{
-		if value.Password==password && value.Username==username{
-			isPresent=true
-			break
-		}
+	var result entity.User
+	fmt.Println("--------checking is user present",username,password)
+	filter := bson.M{"username": username}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	fmt.Println("--------checking is user present",username,password)
+
+	err := userCollection.FindOne(ctx, filter).Decode(&result)
+	
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	} else{
+		isPresent=true
 	}
+	
 	return isPresent
 }
 
-func (s *userService)GetUser(username string, password string)entity.User{
-	var user entity.User;
-	for _,value:=range s.UserList{
-		if value.Password==password && value.Username==username{
-			user=value
-			break
-		}
-	}
-	return user
+func GetUser(username string, password string)entity.User{
+ 
+	var result entity.User
+	 
+	filter := bson.M{"username": username,"password":password}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+ 
+	err := userCollection.FindOne(ctx, filter).Decode(&result)
+	
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	
+	} 
+	return result 
 }
