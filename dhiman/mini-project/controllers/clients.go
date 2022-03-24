1b4fb97bff9ef822f5d153c091889d6f69d63688
dhi13man <dhiman.seal@groww.in>
2022-03-24 10:59:57 +0530

    get client

--- dhiman/mini-project/controllers/clients.go
+++ dhiman/mini-project/controllers/clients.go
@@ -39,3 +39,23 @@ func CreateClient(c *gin.Context) {
   
     c.JSON(http.StatusCreated, gin.H{"message": "success", "data": result})
 }
+
+func GetClient(c *gin.Context) {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    id := c.Param("id")
+    if id == "" {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "id is required"})
+        return
+    }
+
+    var user models.Client
+    err := configs.UsersCollection.FindOne(ctx, models.Client{ID: id}).Decode(&user)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"message": "Error fetching user", "error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "success", "data": user})
+}
