7f7172a5f891507a69afd1f2a7cb029c4e673e51
justamangupta <amangupta8086@gmail.com>
2022-03-27 18:19:32 +0530

    updating db file

--- aman/mini-project/database/dbConnection.go
+++ aman/mini-project/database/dbConnection.go
@@ -13,21 +13,17 @@ import (
 func DBinstance() *mongo.Client {
 	MongoDb := "mongodb://localhost:27017"
 	fmt.Print(MongoDb)
-
 	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-
 	defer cancel()
-
 	err = client.Connect(ctx)
-
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("connected to mongodb")
+	fmt.Println("Connection to DatabaseSuccessful")
 	return client
 }
 
