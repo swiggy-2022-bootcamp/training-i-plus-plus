6cc5d63087c9395bf39db64f2b731482210e48a5
nitesh585 <niteshyadav585@gmail.com>
2022-03-27 13:44:05 +0530

    add kafka example

---
+++ nitesh/training/kafka_example/go.mod
@@ -0,0 +1,11 @@
+module kafkaExample
+
+go 1.17
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2 // indirect
+	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/segmentio/kafka-go v0.4.30 // indirect
+	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.8.2 // indirect
+)
---
+++ nitesh/training/kafka_example/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+const (
+	topic          = "message-log"
+	broker1Address = "localhost:9092"
+	broker2Address = "localhost:9093"
+	broker3Address = "localhost:9094"
+)
+
+func produce(ctx context.Context) {
+	// initialize a counter
+	i := 0
+
+	// intialize the writer with the broker addresses, and the topic
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{broker1Address, broker2Address, broker3Address},
+		Topic:   topic,
+	})
+
+	for {
+		// each kafka message has a key and value. The key is used
+		// to decide which partition (and consequently, which broker)
+		// the message gets published on
+		err := w.WriteMessages(ctx, kafka.Message{
+			Key: []byte(strconv.Itoa(i)),
+			// create an arbitrary message payload for the value
+			Value: []byte("this is message" + strconv.Itoa(i)),
+		})
+		if err != nil {
+			panic("could not write message " + err.Error())
+		}
+
+		// log a confirmation once the message is written
+		fmt.Println("writes:", i)
+		i++
+		// sleep for a second
+		time.Sleep(time.Second)
+	}
+}
+
+func consume(ctx context.Context) {
+	// initialize a new reader with the brokers and topic
+	// the groupID identifies the consumer and prevents
+	// it from receiving duplicate messages
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{broker1Address, broker2Address, broker3Address},
+		Topic:   topic,
+		GroupID: "my-group",
+	})
+	for {
+		// the `ReadMessage` method blocks until we receive the next event
+		msg, err := r.ReadMessage(ctx)
+		if err != nil {
+			panic("could not read message " + err.Error())
+		}
+		// after receiving the message, log its value
+		fmt.Println("received: ", string(msg.Value))
+	}
+}
+func main() {
+	// create a new context
+	ctx := context.Background()
+	// produce messages in a new go routine, since
+	// both the produce and consume functions are
+	// blocking
+	go produce(ctx)
+	consume(ctx)
+}
