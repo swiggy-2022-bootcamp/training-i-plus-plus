797e5869d19cc2238dff40cf9c1556f03c855eb4
justamangupta <amangupta8086@gmail.com>
2022-03-27 23:23:03 +0530

    updating models

--- aman/mini-project/models/productModel.go
+++ aman/mini-project/models/productModel.go
@@ -1,7 +1,12 @@
 package models
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Product struct {
-	ID          string
-	ProductName *string
-	Price       *string
+	ID         primitive.ObjectID `bson:"_id"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Price      *float64           `json:"price" validate:"required"`
+	Product_id string             `json:"food_id"`
 }
--- aman/mini-project/routes/productRouter.go
+++ aman/mini-project/routes/productRouter.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"aman-swiggy-mini-project/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func FoodRoutes(r *gin.Engine) {
+	r.GET("/foods", controllers.GetProducts())
+	r.GET("/foods/:food_id", controllers.GetProduct())
+	r.POST("/foods", controllers.CreateProduct())
+	r.PATCH("/foods/:food_id", controllers.UpdateProduct())
+}
