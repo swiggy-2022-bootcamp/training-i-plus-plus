984a5cf39a78a9bc6884bc8de8b1e4c96224db2d
justamangupta <amangupta8086@gmail.com>
2022-04-03 17:51:15 +0530

    adding kafka producer functionality

--- aman/mini-project/kafka/producer.go
+++ aman/mini-project/kafka/producer.go
@@ -1,12 +1,42 @@
 package kafka
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/segmentio/kafka-go"
+)
 
 const (
 	topic         = "shopTopic"
 	brokerAddress = "localhost:9092"
 )
 
-func Produce(ctx context.Context) {
+func Produce(ctx context.Context, msg string) {
+	i := 0
 
+	l := log.New(os.Stdout, "kafka writer: ", 0)
+	// intialize the writer with the broker addresses, and the topic
+	w := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{brokerAddress},
+		Topic:   topic,
+		// assign the logger to the writer
+		Logger: l,
+	})
+
+	err := w.WriteMessages(ctx, kafka.Message{
+		Key: []byte(strconv.Itoa(i)),
+		// create an arbitrary message payload for the value
+		Value: []byte(msg),
+	})
+	if err != nil {
+		panic("could not write message " + err.Error())
+	}
+
+	// log a confirmation once the message is written
+	fmt.Println("writes:", msg)
+	i++
 }
