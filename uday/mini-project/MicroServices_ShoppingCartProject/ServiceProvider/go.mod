78ed2f223ef315992a6577e988aa1de562d81435
Udaysonu <udaysonubakka123@gmail.com>
2022-04-03 17:18:12 +0530

    [Project]: Add main files in serviceprovider

---
+++ uday/mini-project/MicroServices_ShoppingCartProject/ServiceProvider/go.mod
@@ -0,0 +1,26 @@
+module expertserver
+
+go 1.17
+
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/kafka-go v0.4.31 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.8.4 // indirect
+	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	golang.org/x/text v0.3.5 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.45.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
---
+++ uday/mini-project/MicroServices_ShoppingCartProject/ServiceProvider/main.go
@@ -0,0 +1,74 @@
+package main
+
+import ( pb "expertserver/pb"
+		"context"
+		"fmt"
+		grpc "google.golang.org/grpc"
+        "expertserver/database"
+        "expertserver/entity"
+        "expertserver/config"
+        "go.mongodb.org/mongo-driver/bson"
+        "go.mongodb.org/mongo-driver/mongo"
+        "go.mongodb.org/mongo-driver/bson/primitive"
+        "time"
+        "net"
+        "os"
+        "os/signal"
+     //   "github.com/gin-gonic/gin"
+
+)
+var expertCollection *mongo.Collection = database.GetCollection(database.DB, "experts")
+
+type server struct{
+    pb.UnimplementedServiceServer
+}
+ 
+
+func(*server) ExpertService(ctx context.Context,req *pb.ExpertRequest )(*pb.ExpertResponse, error){
+    fmt.Println("user heartbeat called")
+    id_s:=req.GetExpertid().GetId()
+    var result entity.Expert
+    id,_:=primitive.ObjectIDFromHex(id_s)
+	filter := bson.M{"_id": id}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+ 
+	expertCollection.FindOne(ctx, filter).Decode(&result)
+	reactStruct:=[]*pb.Ratingstruct{};
+    for _,val:=range result.Reviews{
+        reactStruct=append(reactStruct,&pb.Ratingstruct{Rating:int32(val.Rating),Review:val.Review,})
+    }
+    fmt.Println(result)
+    result1:=pb.Expert{
+        Id:result.Id.Hex(),
+        Username:result.Username,
+        Skill:result.Skill,
+        Email:result.Email,
+        Isavailable:result.IsAvailable,
+        Served:int32(result.Served),
+        Rating:float32(result.Rating),
+        Location:int64(result.Location),
+        Ratingstruct:reactStruct,
+    }
+    nameResponse:=pb.ExpertResponse{
+        Expert:&result1,
+    }
+    return &(nameResponse),nil
+}
+ 
+func main(){
+    fmt.Println("Server started")
+    config.InitKafka()
+    opts:=[]grpc.ServerOption{}
+    s:=grpc.NewServer(opts...)
+    pb.RegisterServiceServer(s,&server{})
+    Listener,_:=net.Listen("tcp","localhost:8082")
+    s.Serve(Listener)
+    fmt.Println("welcome")
+    ch:=make(chan os.Signal,1)
+    signal.Notify(ch,os.Interrupt)
+    <-ch
+    fmt.Println("closing connection")
+
+
+}
