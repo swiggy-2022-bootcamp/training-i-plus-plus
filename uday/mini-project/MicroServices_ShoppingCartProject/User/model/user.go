bf0e43c8bcf0735f746636914a83c65e6ff9282d
Udaysonu <udaysonubakka123@gmail.com>
2022-04-03 17:13:28 +0530

    [Project]: Add model user

---
+++ uday/mini-project/MicroServices_ShoppingCartProject/User/model/user.go
@@ -0,0 +1,13 @@
+package entity
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+type User struct{
+	Id 	primitive.ObjectID	`bson:"_id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string	`json:"email"`
+	Phone int		`json:"phone"`
+	Location int    `json:"location"`
+}
+
