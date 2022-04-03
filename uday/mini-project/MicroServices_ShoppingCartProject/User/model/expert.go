58c478f946a1239ef79e25e029bda3d1526d37f0
Udaysonu <udaysonubakka123@gmail.com>
2022-04-03 17:12:31 +0530

    [Project]: Add model expert

---
+++ uday/mini-project/MicroServices_ShoppingCartProject/User/model/expert.go
@@ -0,0 +1,20 @@
+package entity
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+type Expert struct{
+	Id 	primitive.ObjectID	`bson:"_id"`
+	Username     	string	`json:"username"`
+	Skill   	    string	`json:"skill"`
+	Email 			string	`json:"email"`
+	IsAvailable     bool	`json:"isavailable"`
+	Served			int		`json:"served"`
+	Rating 			float64 `json:"rating"`
+	Location        int     `json:"location"`
+	Reviews         []RatingStruct  `json:"reviews"`
+}
+
+type RatingStruct struct{
+	Rating int `json:"rating"`
+	Review string `json:"review"`
+}
