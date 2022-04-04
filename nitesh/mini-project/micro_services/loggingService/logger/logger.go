96c6cd9978a63f7ab627e5822136b968fdbb0c82
nitesh585 <niteshyadav585@gmail.com>
2022-04-04 12:44:13 +0530

    (micro-service) add loggingService

---
+++ nitesh/mini-project/micro_services/loggingService/logger/logger.go
@@ -0,0 +1,37 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+var logger *logrus.Logger
+
+// Singleton Pattern
+func GetLogger() *logrus.Logger {
+	var once sync.Once
+	if logger == nil {
+		once.Do(
+			func() {
+				logger = logrus.New()
+
+				src, err := os.OpenFile("logs.log", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+
+				if err != nil {
+					fmt.Print(err.Error())
+					fmt.Print("unable to create user.log file")
+				}
+
+				multiWriter := io.MultiWriter(os.Stdout, src)
+
+				logger.SetFormatter(&logrus.JSONFormatter{})
+				logger.SetOutput(multiWriter)
+			})
+	}
+
+	return logger
+}
