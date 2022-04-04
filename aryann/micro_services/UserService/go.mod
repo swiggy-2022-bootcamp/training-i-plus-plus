c095cb78a4f3540d0e94d243b519513b4b1571c1
Aryann Dhir <aryanndhir.1@gmail.com>
2022-04-04 12:12:35 +0530

    added sonarqube

--- aryann/micro_services/UserService/go.mod
+++ aryann/micro_services/UserService/go.mod
@@ -13,6 +13,12 @@ require (
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+)
+
+require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
@@ -32,9 +38,10 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.9 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/labstack/gommon v0.3.1
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
---
+++ aryann/micro_services/UserService/helpers/authservice_test.go
@@ -0,0 +1,59 @@
+package helpers
+
+import (
+	"UserService/models"
+	"testing"
+
+	"github.com/labstack/gommon/log"
+)
+
+func ValidateRequest(user models.SignUp) bool {
+	if user.Email == "" || user.Password == "" || user.Username == "" {
+		log.Error("Either user email or password missing from sign in request")
+		return true
+	}
+
+	return false
+}
+
+func TestSignIn(t *testing.T) {
+
+	cases := []struct {
+		name          string
+		args          models.SignUp
+		expectedError bool
+	}{
+		{
+			name: "ValidSignUp",
+			args: models.SignUp{
+				Username: "varun",
+				Email:    "vkj@gmail.com",
+				TypeOf:   "user",
+				Password: "varun",
+			},
+			expectedError: false,
+		},
+		{
+			name: "MissingUsernameSignUp",
+			args: models.SignUp{
+				Username: "",
+				Email:    "am@gmail.com",
+				TypeOf:   "student",
+				Password: "aditi",
+			},
+			expectedError: true,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := ValidateRequest(c.args)
+			if c.expectedError && err == false {
+				t.Error("Expected an error but didn't get one")
+			}
+			if !c.expectedError && err != false {
+				t.Errorf("Expected no error but got %v", err)
+			}
+		})
+	}
+}
