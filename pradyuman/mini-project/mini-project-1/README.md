d87e9e0093d3cbbe17b6fb78ec583979313907d9
PradyumanAgrawal <pa14@iitbbs.ac.in>
2022-04-04 21:27:57 +0530

    removed unused files

--- pradyuman/mini-project/mini-project-1/README.md
+++
@@ -1,4 +0,0 @@
-zookeeper-server-start.bat ../../config/zookeeper.properties
-kafka-server-start.bat ../../config/server.properties
-
-cd C:\kafka\bin\windows
--- pradyuman/mini-project/mini-project-1/responses/product_response.go
+++
@@ -1,7 +0,0 @@
-package responses
-
-type ProductResponse struct {
-    Status  int                    `json:"status"`
-    Message string                 `json:"message"`
-    Data    map[string]interface{} `json:"data"`
-}
